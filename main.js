19b41c253e4e0f4a8282528f5143d58b84ce2aed
Facundo.Cretton <facundo.cretton@gmail.com>
2023-02-27 19:15:16 -0300

    avance y correcciones

--- main.js
+++ main.js
@@ -232,6 +232,7 @@ function smoothScroll() {
 // });
 // -----------------PRUEBA---------------
 const toggleMenu = (event) => {
+  console.log("Toggle menu");
   if (event.target.matches('[data-logo]') || event.target.closest('[data-logo]')) {
     return;
   }
@@ -244,6 +245,27 @@ const toggleMenu = (event) => {
   overlay.classList.toggle("show-overlay");
 };
 
+barsBtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  setTimeout(() => {
+    toggleMenu(event);
+  }, 100);
+});
+
+document.addEventListener("click", (event) => {
+  console.log("doble click");
+  if (
+    event.target !== barsBtn &&
+    !barsMenu.contains(event.target) &&
+    !cartMenu.contains(event.target)
+  ) {
+    barsMenu.classList.remove("open-menu");
+    cartMenu.classList.remove("open-cart");
+    overlay.classList.remove("show-overlay");
+  }
+});
+
+// ----------------------
 
 const toggleCart = () => {
 	cartMenu.classList.toggle("open-cart");
@@ -299,7 +321,7 @@ const init =()=>{
 	barsMenu.addEventListener("click", closeOnClick);
 	window.addEventListener("scroll", closeOnScroll);
 	overlay.addEventListener("click", closeOnOverlayClick);
-  barsBtn.addEventListener("click", toggleMenu);
+  // barsBtn.addEventListener("click", toggleMenu);
   barsBtn.addEventListener('click', event => {
     event.preventDefault();
   });
