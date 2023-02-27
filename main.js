e19fe9c4ff93d1a3bf7ff04fe9cbcbadc030d561
Facundo.Cretton <facundo.cretton@gmail.com>
2023-02-27 06:33:33 -0300

    carrito

--- main.js
+++ main.js
@@ -11,6 +11,24 @@ const bodyElement = document.querySelector("body");
 const sunIcon = document.querySelector("#sun-icon");
 const moonIcon = document.querySelector("#moon-icon");
 const navLinks = document.querySelectorAll('.navbar a');
+// -------
+const buyBtn = document.querySelector(".btn-buy");
+// Botón para abrir y cerrar carrito
+//Burbuja de cantidad de productos en el carrito
+const cartBubble = document.querySelector(".cart-bubble");
+const cartBtn = document.querySelector(".cart-label");
+// Botón para abrir y cerrar menú
+const barsBtn = document.querySelector(".menu-label");
+// Carrito
+const cartMenu = document.querySelector(".cart");
+//  Menú (Hamburguesa)
+const barsMenu = document.querySelector(".navbar-list");
+//  Overlay para tirar facha abajo del menú hamburguesa y el cart.
+const overlay = document.querySelector(".overlay");
+//  Modal de agregado al carrito.
+const successModal = document.querySelector(".add-modal");
+//  Modal de agregado al carrito.
+const deleteBtn = document.querySelector(".btn-delete");
 
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
@@ -209,6 +227,51 @@ function smoothScroll() {
 //     renderCategoryProducts(categoria);
 //   });
 // });
+// -----------------PRUEBA---------------
+const toggleMenu = () => {
+	barsMenu.classList.toggle("open-menu");
+	if (cartMenu.classList.contains("open-cart")) {
+		cartMenu.classList.remove("open-cart");
+		return;
+	}
+	overlay.classList.toggle("show-overlay");
+};
+
+const toggleCart = () => {
+	cartMenu.classList.toggle("open-cart");
+	if (barsMenu.classList.contains("open-menu")) {
+		barsMenu.classList.remove("open-menu");
+		return;
+	}
+	overlay.classList.toggle("show-overlay");
+};
+
+const closeOnClick = (e) => {
+	if (!e.target.classList.contains("navbar-list a")) {
+		return;
+	}
+	barsMenu.classList.remove("open-menu");
+	overlay.classList.remove("show-overlay");
+};
+
+const closeOnScroll = () => {
+	if (
+		!barsMenu.classList.contains("open-menu") &&
+		!cartMenu.classList.contains("open-cart")
+	) {
+		return;
+	}
+	barsMenu.classList.remove("open-menu");
+	cartMenu.classList.remove("open-cart");
+	overlay.classList.remove("show-overlay");
+};
+
+const closeOnOverlayClick = () => {
+	barsMenu.classList.remove("open-menu");
+	cartMenu.classList.remove("open-cart");
+	overlay.classList.remove("show-overlay");
+};
+
 
 
 
@@ -224,7 +287,10 @@ const init =()=>{
     toggleTheme();
   }
   smoothScroll();
-
+  cartBtn.addEventListener("click", toggleCart);
+	barsMenu.addEventListener("click", closeOnClick);
+	window.addEventListener("scroll", closeOnScroll);
+	overlay.addEventListener("click", closeOnOverlayClick);
 }
 init();
 
